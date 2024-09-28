539845437969e3b76f3d771ffcfdbba7b4ee2a0f
tausif892 <63648692+tausif892@users.noreply.github.com>
2024-09-28 13:11:50 +0530

    Login changed

--- app.py
+++ app.py
@@ -115,5 +115,5 @@ def nutri():
         else:
             return jsonify({'error': 'User not found'}), 404
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
